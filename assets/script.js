a9b3101b6ae2950cca15781ddf413579ae0375e5
Grey P <greytpearson@gmail.com>
2023-01-26 13:52:19 -0700

    fixed search function, creating functions to display weather and save the searchs user has made

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,25 @@
 let apiKey = "abcc66694ca1018668923524441c6829"
-
-// let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&appid=" + apiKey
-let searchBar = $('#searchBar')
 let content = $('button')
-let city = ''
+// make var for each used element and have to do creating element funciton
 
-// fetch(queryURL)
-//   .then((response) => response.json())
-//   .then((data) => console.log(data));
+// api reponse.list has 40 objects
+// each usable object on api response.list[0, 5, 13, 21, 29]
 
-// content.on('click', function(){
-//     console.log('test')
-//     console.log(searchBar.val())
-//     city = searchBar.val()
-//     fetch(queryURL)
-//   .then((response) => response.json())
-//   .then((data) => console.log(data));
-// })
-
-
-content.on('click', function(){
-    const searchTerm = searchBar.value
-    console.log(searchTerm)
-    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey
-    fetch(queryURL).then((response) => response.json()).then((data) => console.log(data));
+content.on('click', function () {
+    let searchBar = document.getElementById("searchBar").value;
+    console.log(searchBar)
+    let city = searchBar
+    let queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`
+    fetch(queryURL).then((response) => response.json()).then((data) => displayWeather(data));
+    recordSearch(city)
 })
+
+// only appends data to html for weather
+function displayWeather(data) {
+
+}
+
+// only to save searches and create the list elements
+function recordSearch(city) {
+
+}
