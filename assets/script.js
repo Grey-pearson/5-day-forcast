273b57c2520b1ccabada17829face940ac5c6aa2
Grey P <greytpearson@gmail.com>
2023-01-26 15:41:59 -0700

    fixing bug

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-let apiKey = "abcc66694ca1018668923524441c6829"
+let apiKey = ""
 let content = $('button')
 // make var for each used element and have to do creating element funciton
 
@@ -6,9 +6,7 @@ let content = $('button')
 // each usable object on api response.list[0, 5, 13, 21, 29]
 
 content.on('click', function () {
-    let searchBar = document.getElementById("searchBar").value;
-    console.log(searchBar)
-    let city = searchBar
+    let city = document.getElementById("searchBar").value;
     let queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`
     fetch(queryURL).then((response) => response.json()).then((data) => displayWeather(data));
     recordSearch(city)
@@ -16,7 +14,16 @@ content.on('click', function () {
 
 // only appends data to html for weather
 function displayWeather(data) {
-
+    let day0 = data.list[0]
+    let day1 = data.list[5]
+    let day2 = data.list[13]
+    let day3 = data.list[21]
+    let day4 = data.list[29]
+    console.log(day0)
+    console.log(day1)
+    console.log(day2)
+    console.log(day3)
+    console.log(day4)
 }
 
 // only to save searches and create the list elements
