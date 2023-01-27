e2c5ca62e6c98029d0847889b21337270d8ed7aa
Grey P <greytpearson@gmail.com>
2023-01-27 12:42:58 -0700

    modifying script.js

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ let content = $('button')
 // make var for each used element and have to do creating element funciton
 
 // api reponse.list has 40 objects
-// each usable object on api response.list[0, 5, 13, 21, 29]
+// each usable object on api response.list[0, 8, 16, 34, 42]
 
 content.on('click', function () {
     let city = document.getElementById("searchBar").value;
@@ -15,10 +15,10 @@ content.on('click', function () {
 // only appends data to html for weather
 function displayWeather(data) {
     let day0 = data.list[0]
-    let day1 = data.list[5]
-    let day2 = data.list[13]
-    let day3 = data.list[21]
-    let day4 = data.list[29]
+    let day1 = data.list[8]
+    let day2 = data.list[16]
+    let day3 = data.list[34]
+    let day4 = data.list[40]
     console.log(day0)
     console.log(day1)
     console.log(day2)
@@ -28,5 +28,6 @@ function displayWeather(data) {
 
 // only to save searches and create the list elements
 function recordSearch(city) {
+
 
 }
